05cb93ef70fde1283fc1ee3fb37a51b828a4d27f
teonateona12 <teona.piranishvili1@gmail.com>
2022-05-16 17:15:39 +0400

    homework_2

---
+++ script.js
@@ -0,0 +1,123 @@
+//Task 1
+/*
+    შექმენით ფუნქცია რომელსაც აქვს ერთი პარამეტრი . 
+    ფუნქციას გადაეცით ობიექტი არგუმეტნად 
+    
+    let sampleObject = {
+        isItarable : false,
+        sampleArray : [12,63,21,34,98,57]
+    }
+    თუ მიღებული არგუმენტის isItarable მნიშვნელობა არის ჭეშმარიტი მაშინ დალოგეთ თითოეული ელემენტი
+    თუ არ არის ჭეშმარიტი გამოიტანეთ წინადადება "provided array isn't itarable"
+
+*/
+
+function myFunction(){
+    let sampleObject = {
+        isItarable : false,
+        sampleArray : [12,63,21,34,98,57]
+    }
+    if(sampleObject.isItarable == true){
+        return sampleObject.sampleArray
+    }else{
+        return "provided array isn't itarable"
+    }
+}
+console.log(myFunction())
+
+
+
+
+
+//Task 2
+/*
+    შექმენით ფუნქცია checkPythagoras რომელიც მიიღებს სამ პარამეტრს x,y,z .
+    ფუნქციამ უნდა შეამოწმოს თუ აღნიშნული რიცხვების რომელიმე კომბინაცია მაინც
+    აკმაყოფილებს პითაგორას თეორემის პირობას a^2 + b^2 = c^2;
+    თუ კი ეს პირობა დაკმაყოფილდა ფუნქციამ უნდა დააბრუნოს true 
+    წინააღმდეგ შემთხვევაში false
+    
+*/
+
+function checkPythagoras(x,y,z){
+    if(z*z+y*y==x*x){
+        return true
+    }else if(y*y+x*x==z*z){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(checkPythagoras(3,4,5))
+
+
+
+
+
+//Task 3
+/*
+    დაწერეთ ფუნქცია minMax რომელიც იღებს 1 მასივის ტიპის პარამეტრს 
+    და დააბრუნებს მასივის ელემენტებს შორის მაქსიმალურს და მინიმალურს ელემენტს .
+    მაგ : [2,14,25,75,11,6] თუ კი ამ მასივს გადავცემთ არგუმენტის სახით ფუნქციამ უნდა
+    დააბრუნოს "Min value is 2 and Max value is 75"
+*/
+
+
+
+function minMax(x){
+    return 'Min value is ' + Math.min(...x ) + " " + 'Max value is ' + Math.max(...x)
+}
+
+console.log(minMax([60,30,25,5]));
+
+//Task 4
+
+/*
+დაწერეთ ფუნქცია რომელმაც გადაცემული კუთხის მნიშვნელობით უნდა დააბრუნოს
+თუ რა ტიპის კუთხეა . 
+კუთხის სახეები:
+Acute angle: An angle between 0 and 90 degrees.
+Right angle: An 90 degree angle.
+Obtuse angle: An angle between 90 and 180 degrees.
+Straight angle: A 180 degree angle.
+
+*/
+
+function nameAngle(angle){
+    if(angle<90){
+        return "Acute angle";
+    }else if(angle==90){
+        return "Right angle";
+    }else if(angle<180 && angle>90){
+        return "Obtuse angle";
+    }else if(angle == 180){
+        return "Straight angle";
+    }
+}
+console.log(nameAngle(160))
+
+
+
+
+
+??
+//Task 5
+/*
+    დაწერეთ ფუნქცია checkStudentGrade რომელიც მიიღებს ობიექტების მასივს. 
+    მაგალითად [{name: student1; grade : 91}, {name: student2; grade : 71}, {name: student3; grade : 45} ]
+    ფუნქციამ სტუდენტის ქულის მიხედვით უნდა განსაზღვროს საფინალო შედეგი finalResult 
+    ქვემოთ არსებული ცხრილის მიხედვით და ელემენტის ობიექტში დაამატოს propertyს სახით
+    მიღებული მასივი დააბრუნებინეთ ფუნქციას .
+
+    0-50 = F
+    51-60 = E
+    61-70 = D
+    71-80 = C
+    81-100 = A
+
+    საორიენტაციოდ შედეგი აღნიშNული მაგალითის მიხედვით უნდა იყოს
+    [{name: student1; grade : 91 ; finalResult : 'A'}, {name: student2; grade : 71,finalResult : 'C'}, {name: student3; grade : 45, finalResult : 'F'} ]
+
+
+*/
+
